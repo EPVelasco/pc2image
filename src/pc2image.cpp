45057b88213bed82cbfd1c5d907497119f36c2ce
Edison Velasco Sánchez <35384396+EPVelasco@users.noreply.github.com>
2022-08-17 21:39:38 +0200

    Update pc2image.cpp

--- src/pc2image.cpp
+++ src/pc2image.cpp
@@ -9,21 +9,25 @@
 #include <pcl/filters/filter.h>
 #include <opencv2/core/core.hpp>
 #include <math.h>
-
+#include <iostream>
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
 
 ros::Publisher imgD_pub;
 boost::shared_ptr<pcl::RangeImageSpherical> rngSpheric;
 pcl::RangeImage::CoordinateFrame coordinate_frame = pcl::RangeImage::LASER_FRAME;
 
-
-float maxlen =500;
+float maxlen =100;
 float minlen = 0.1;
 float angular_resolution_x = 0.25f;
-float angular_resolution_y = 0.85f;
+float angular_resolution_y = 2.05f;
 float max_angle_width= 360.0f;
 float max_angle_height = 360.0f;
+
+float z_max = 100.0f;
+float z_min = 100.0f;
+
 std::string pcTopic = "/velodyne_points";
 
 
@@ -43,33 +47,54 @@ void callback(const PointCloud::ConstPtr& msg_pointCloud)
       if(distance<minlen || distance>maxlen)
           continue;
      cloud_out->push_back(cloud_in->points[i]);
+
+     
   }
 
   rngSpheric->pcl::RangeImage::createFromPointCloud(*cloud_out, pcl::deg2rad(angular_resolution_x), pcl::deg2rad(angular_resolution_y),
-                                       pcl::deg2rad(max_angle_height), pcl::deg2rad(max_angle_height),
-                                       Eigen::Affine3f::Identity(), coordinate_frame, 0.0, 0.0f, 0);
+                                       pcl::deg2rad(max_angle_width), pcl::deg2rad(max_angle_height),
+                                       Eigen::Affine3f::Identity(), coordinate_frame, 0.0f, 0.0f, 0);
+                                
+  rngSpheric->header.frame_id = msg_pointCloud->header.frame_id;
+  rngSpheric->header.stamp    = msg_pointCloud->header.stamp;
 
-  rngSpheric->header.frame_id = cloud_out->header.frame_id;
-  rngSpheric->header.stamp    = cloud_out->header.stamp;
-
-  int cols = rngSpheric->width;
+  
+  int cols = (rngSpheric->width) * 2;
   int rows = rngSpheric->height;
-  cv::Mat dephtImage = cv::Mat::zeros(rows, cols, cv_bridge::getCvType("mono16"));;
+  
 
-  for (int i=0; i<cols; ++i)
-      for (int j=0; j<rows; ++j)
+  cv::Mat dephtImage =  cv::Mat::zeros(rows, cols, cv_bridge::getCvType("mono16"));
+
+  unsigned short range = 0 , z_range = 0;
+
+  for (int i=0; i< int(cols/2); ++i)
+   {
+       
+      for (int j=0; j<rows ; ++j)
       {
-        float r = rngSpheric->getPoint(i, j).range;
-        if(std::isinf(r) || r<minlen || r>maxlen){
+        float r =  rngSpheric->getPoint(i, j).range;     
+        float zz = rngSpheric->getPoint(i, j).z + 50.0;          
+        
+        if(std::isinf(r) || r<minlen || r>maxlen || std::isnan(zz)){
+
             continue;
         }
-        unsigned short range = 1-(pow(2,16)/ (maxlen - minlen))*(r-minlen);
-        dephtImage.at<ushort>(j, i) = range;
-    }
 
-   sensor_msgs::ImagePtr image_msg;
-   image_msg = cv_bridge::CvImage(std_msgs::Header(), "mono16", dephtImage).toImageMsg();
-   imgD_pub.publish(image_msg);
+        
+        range = 1-(pow(2,16)/ (maxlen - minlen))*(r-minlen);   
+        dephtImage.at<ushort>(j, i) = range;       
+        z_range = (zz/z_max)* pow(2,16);
+
+        dephtImage.at<ushort>(j, i+int(cols/2)) = z_range;
+              
+    }
+  }
+
+  sensor_msgs::ImagePtr image_msg;
+  image_msg = cv_bridge::CvImage(std_msgs::Header(), "mono16", dephtImage).toImageMsg();
+
+  image_msg->header = pcl_conversions::fromPCL(msg_pointCloud->header);
+  imgD_pub.publish(image_msg);
 
 }
 
@@ -91,6 +116,7 @@ int main(int argc, char** argv)
   ros::Subscriber sub = nh.subscribe<PointCloud>(pcTopic, 10, callback);
   rngSpheric = boost::shared_ptr<pcl::RangeImageSpherical>(new pcl::RangeImageSpherical);
   imgD_pub = nh.advertise<sensor_msgs::Image>("/depth_image", 10);
+
   ros::spin();
   return 0;
 }
